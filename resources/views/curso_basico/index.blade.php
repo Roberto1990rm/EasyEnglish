d9325b5aff2eb6eccee1411a8b58fe7c17924b6a
Roberto1990rm <robertoramirezmoreno@gmail.com>
2025-03-11 20:19:40 -0300

    test

--- resources/views/curso_basico/index.blade.php
+++ resources/views/curso_basico/index.blade.php
@@ -1,48 +1,102 @@
 @extends('layouts.app')
 
 @section('content')
-<div  class="mt-5">
+<div class="mt-5">
     <h1 class="p1Home text-center">Curso Básico</h1>
 
     @auth
-    
     <div class="text-center">
         <a href="{{ route('curso_basico.create') }}" class="btn btn-primary mb-3">Crear Nueva Lección</a>
     </div>
     @endauth
        
-    <div class="row">
-        @foreach($lecciones as $leccion)
-            <div class="col-md-4">
-                <div class="card mb-4 ms-2">
-                    <img src="{{ asset('storage/' . $leccion->image) }}" class="card-img-top" alt="{{ $leccion->title }}">
-                    <div class="card-body">
-                        <h5 class="card-title">{{ $leccion['title'] }}</h5>
-                        <p class="card-text">{{ $leccion['description'] }}</p>
-                        <a href="{{ route('curso.basico.show', $leccion['id']) }}" class="btn btn-primary">Ver más</a>
+    <!-- Contenedor con padding horizontal para separar del borde -->
+    <div class="container px-5">
+        <div class="row">
+            @foreach($lecciones as $leccion)
+                <div class="col-md-4">
+                    <div class="card mb-4 ms-2">
+                        <img style="height: 170px;" src="{{ asset('storage/' . $leccion->image) }}" class="card-img-top" alt="{{ $leccion->title }}">
+                        <div class="card-body">
+                            <h5 class="card-title">{{ $leccion['title'] }}</h5>
+                            <p class="card-text">{{ $leccion['description'] }}</p>
+                            <div class="d-flex justify-content-center gap-2">
+                                <!-- Botón "Ver más" como icono -->
+                                <a href="{{ route('curso.basico.show', $leccion['id']) }}" class="btn btn-primary btn-sm" title="Ver más">
+                                    <i class="fas fa-eye"></i>
+                                </a>
+                        
+                                <!-- Botón "Eliminar" como icono -->
+                                <form action="{{ route('curso.basico.destroy', $leccion->id) }}" method="POST" onsubmit="return confirm('¿Estás seguro de que deseas eliminar esta lección?');">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button type="submit" class="btn btn-danger btn-sm" title="Eliminar">
+                                        <i class="fas fa-trash-alt"></i>
+                                    </button>
+                                </form>
+                            </div>
+                        </div>
                     </div>
-
-                    <div class="ms-3 mb-1">
-                     <!-- Botón de eliminar -->
-                <form action="{{ route('curso.basico.destroy', $leccion->id) }}" method="POST" class="d-inline">
-                    @csrf
-                    @method('DELETE')
-                    <button type="submit" class="btn btn-danger" onclick="return confirm('¿Estás seguro de que deseas eliminar esta lección?');">
-                        Eliminar
-                    </button>
-                </form>
-            </div>
                 </div>
-                
-            </div>
-        @endforeach
+            @endforeach
+        </div>
     </div>
 
     <div class="text-center">
-    <a href="{{ url('/') }}" class="btn btn-info">
-        <i class="fas fa-home"></i> Inicio
-    </a>
+        <a href="{{ url('/') }}" class="btn btn-info">
+            <i class="fas fa-home"></i> Inicio
+        </a>
+    </div>
 </div>
+@endsection
+@extends('layouts.app')
 
+@section('content')
+<div class="mt-5">
+    <h1 class="p1Home text-center">Curso Básico</h1>
+
+    @auth
+    <div class="text-center">
+        <a href="{{ route('curso_basico.create') }}" class="btn btn-primary mb-3">Crear Nueva Lección</a>
+    </div>
+    @endauth
+       
+    <!-- Contenedor con padding horizontal para separar del borde -->
+    <div class="container px-5">
+        <div class="row">
+            @foreach($lecciones as $leccion)
+                <div class="col-md-4">
+                    <div class="card mb-4 ms-2">
+                        <img style="height: 170px;" src="{{ asset('storage/' . $leccion->image) }}" class="card-img-top" alt="{{ $leccion->title }}">
+                        <div class="card-body">
+                            <h5 class="card-title">{{ $leccion['title'] }}</h5>
+                            <p class="card-text">{{ $leccion['description'] }}</p>
+                            <div class="d-flex justify-content-center gap-2">
+                                <!-- Botón "Ver más" como icono -->
+                                <a href="{{ route('curso.basico.show', $leccion['id']) }}" class="btn btn-primary btn-sm" title="Ver más">
+                                    <i class="fas fa-eye"></i>
+                                </a>
+                        
+                                <!-- Botón "Eliminar" como icono -->
+                                <form action="{{ route('curso.basico.destroy', $leccion->id) }}" method="POST" onsubmit="return confirm('¿Estás seguro de que deseas eliminar esta lección?');">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button type="submit" class="btn btn-danger btn-sm" title="Eliminar">
+                                        <i class="fas fa-trash-alt"></i>
+                                    </button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    </div>
+
+    <div class="text-center">
+        <a href="{{ url('/') }}" class="btn btn-info">
+            <i class="fas fa-home"></i> Inicio
+        </a>
+    </div>
 </div>
 @endsection
