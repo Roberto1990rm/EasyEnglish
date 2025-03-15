747cc5afef93633f6aa278ba889266f88f932c9d
Roberto1990rm <robertoramirezmoreno@gmail.com>
2025-03-15 10:44:24 -0300

    pronouns delete added

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -28,51 +28,55 @@
     </div>
 </div>
 
-<div class="container " id="cursos">
-    <div class="row mt-5">
-        <div class="col-lg-4 col-md-6 mb-4 ">
-            <div class="card shadow-sm">
-                <img src="{{ asset('images/basico1.jpg') }}" class="card-img-top" style="height: auto;" alt="Curso de Inglés">
-                <div class="card-body1 text-center">
-                    <h5 class="card-title mt1">Curso Básico de Inglés</h5>
-                    <p class="card-text">Aprende los conceptos básicos del inglés con clases interactivas y dinámicas.</p>
-                    <div class="text-center">
-                        <a href="{{ route('curso.basico.index') }}" class="btn btn-primary btn-sm mb-2" title="Ver más">
-                            <i class="fas fa-eye"></i>
-                        </a>
+     <<div class="container" id="cursos">
+        <div class="row mt-5">
+            <div class="col-lg-4 col-md-6 mb-4">
+                <div class="card shadow-sm h-100">
+                    <img src="{{ asset('images/basico1.jpg') }}" class="card-img-top" style="height:200px; object-fit:cover;" alt="Curso de Inglés">
+                    <div class="card-body d-flex flex-column text-center">
+                        <h5 class="card-title mt-1">Curso Básico de Inglés</h5>
+                        <p class="card-text">Aprende los conceptos básicos del inglés con clases interactivas y dinámicas.</p>
+                        <div class="mt-auto">
+                            <a href="{{ route('curso.basico.index') }}" class="btn btn-primary btn-sm mb-2" title="Ver más">
+                                <i class="fas fa-eye"></i>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+            <div class="col-lg-4 col-md-6 mb-4">
+                <div class="card shadow-sm h-100">
+                    <img src="{{ asset('images/basico2.jpg') }}" class="card-img-top" style="height:200px; object-fit:cover;" alt="Curso de Inglés">
+                    <div class="card-body d-flex flex-column text-center">
+                        <h5 class="card-title mt-1">Curso Intermedio de Inglés</h5>
+                        <p class="card-text">Amplía tus conocimientos del inglés con lecciones y ejercicios de nivel intermedio.</p>
+                        <div class="mt-auto">
+                            <a href="#" class="btn btn-primary btn-sm mb-2" title="Ver más">
+                                <i class="fas fa-eye"></i>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+            <div class="col-lg-4 col-md-6 mb-4">
+                <div class="card shadow-sm h-100">
+                    <img src="{{ asset('images/basico3.jpg') }}" class="card-img-top" style="height:200px; object-fit:cover;" alt="Curso de Inglés">
+                    <div class="card-body d-flex flex-column text-center">
+                        <h5 class="card-title mt-1">Curso Avanzado de Inglés</h5>
+                        <p class="card-text">Perfecciona tu inglés con clases avanzadas centradas en fluidez y comprensión.</p>
+                        <div class="mt-auto">
+                            <a href="#" class="btn btn-primary btn-sm mb-2" title="Ver más">
+                                <i class="fas fa-eye"></i>
+                            </a>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-        <div class="col-lg-4 col-md-6 mb-4">
-            <div class="card shadow-sm">
-                <img src="{{ asset('images/basico2.jpg') }}" class="card-img-top" alt="Curso de Inglés" style="height: auto;">
-                <div class="card-body1 text-center">
-                    <h5 class="card-title mt-1">Curso Intermedio de Inglés</h5>
-                    <p class="card-text">Amplía tus conocimientos del inglés con lecciones y ejercicios de nivel intermedio.</p>
-                    <div class="text-center">
-                        <a href="#" class="btn btn-primary btn-sm mb-2" title="Ver más">
-                            <i class="fas fa-eye"></i>
-                        </a>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="col-lg-4 col-md-6 mb-4">
-            <div class="card shadow-sm">
-                <img src="{{ asset('images/basico3.jpg') }}" class="card-img-top" style="height: auto;" alt="Curso de Inglés">
-                <div class="card-body1 text-center">
-                    <h5 class="card-title mt-1">Curso Avanzado de Inglés</h5>
-                    <p class="card-text">Perfecciona tu inglés con clases avanzadas centradas en fluidez y comprensión.</p>
-                    <div class="text-center">
-                        <a href="#" class="btn btn-primary btn-sm mb-2" title="Ver más">
-                            <i class="fas fa-eye"></i>
-                        </a>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <!-- Dashboard -->
+    </div>
+    !-- Dashboard -->
        
         <!-- Fin Dashboard -->
     </div>
