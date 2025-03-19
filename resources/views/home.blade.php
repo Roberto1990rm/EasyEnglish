eb4aaab3b616ddeeae720625430ff545829ed667
Roberto1990rm <robertoramirezmoreno@gmail.com>
2025-03-19 13:40:22 -0300

    Guardando cambios locales antes de actualizar

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -25,7 +25,7 @@
 
         <div class="item  shadow-md p-6 rounded-lg">
 
-            <h1 class="text-xl font-bold text-gray-800">Bienvenido a MyWebsite x</h1>
+            <h1 class="text-xl font-bold text-gray-800"> Bienvenido a MyWebsite x</h1>
             <p class="text-gray-600 mt-4">Tu sitio moderno con Laravel y Blade</p>
         </div>
     </div>
