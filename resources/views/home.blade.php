c40ad3fc75072ad0aa3cff3b22f7420503e3b414
Roberto1990rm <robertoramirezmoreno@gmail.com>
2025-03-19 13:36:54 -0300

    SS

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -22,8 +22,10 @@
             <p class="text-gray-600 mt-4">Tu sitio moderno con Laravel y Blade</p>
         </div>
 
+
         <div class="item  shadow-md p-6 rounded-lg">
-            <h1 class="text-xl font-bold text-gray-800">Bienvenido a MyWebsite</h1>
+
+            <h1 class="text-xl font-bold text-gray-800">Bienvenido a MyWebsite x</h1>
             <p class="text-gray-600 mt-4">Tu sitio moderno con Laravel y Blade</p>
         </div>
     </div>
