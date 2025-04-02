a24adfe31c4f0cc90ba10b97294c3b3f208632bf
Roberto1990rm <robertoramirezmoreno@gmail.com>
2025-04-02 20:16:36 -0300

    SS

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -14,7 +14,6 @@ class ContactController extends Controller
             'email' => 'required|email',
             'message' => 'required|string',
         ]);
-
         
         // Enviar el correo
         Mail::send('emails.contact', [
